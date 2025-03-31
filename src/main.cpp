d9e37bd3d2483a2abd7ff9876fceaadbce9ea51b
Jacob Shing <zhanhoshing@icloud.com>
2025-03-31 16:16:12 +0800

    feat: checks screen size for min requirements

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,29 @@
-#include <ftxui/dom/elements.hpp>
-#include <ftxui/screen/screen.hpp>
 #include <iostream>
+
+#include <ftxui/screen/screen.hpp>
+#include <ftxui/component/screen_interactive.hpp>
+#include <ftxui/component/component.hpp>
+#include <ftxui/component/captured_mouse.hpp>
  
 int main(void) {
-  using namespace ftxui;
- 
-  // Define the document
-  Element document =
-    hbox({
-      text("left")   | border,
-      text("middle") | border | flex,
-      text("right")  | border,
-    });
- 
-  auto screen = Screen::Create(
-    Dimension::Full(),       // Width
-    Dimension::Fit(document) // Height
-  );
-  Render(screen, document);
-  screen.Print();
- 
-  return EXIT_SUCCESS;
+    const int minWidth = 102, minHeight = 30; // TODO: replace with defined constants in core
+    ftxui::Dimensions terminalDim = ftxui::Dimension::Full();
+
+    if (terminalDim.dimx < minWidth || terminalDim.dimy < minHeight) {
+        auto document = ftxui::hbox({
+                ftxui::text("ERROR") | ftxui::bold | ftxui::blink | ftxui::color(ftxui::Color::Red),
+                ftxui::separator(),
+                ftxui::text("Your terminal is too small. Minimum requirement: 102x30.") | ftxui::xflex,
+        }) | ftxui::borderRounded;
+        auto screen = ftxui::Screen::Create(
+            ftxui::Dimension::Full(),
+            ftxui::Dimension::Fit(document)
+        );
+        ftxui::Render(screen, document);
+        screen.Print();
+
+        return 1;
+    }
+
+    return 0;
 }
