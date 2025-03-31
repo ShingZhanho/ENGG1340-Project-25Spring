da13dee227809f2c562672e1e26f3bd1926d907a
Jacob Shing <zhanhoshing@icloud.com>
2025-03-31 12:27:18 +0800

    feat: implement Air, Wall and ModWall classes

--- include/core/entity.hpp
+++ include/core/entity.hpp
@@ -7,6 +7,11 @@
 
 namespace core {
 
+    class Air;
+    class Wall;
+    class MobWall;
+    class Arena;
+
     typedef struct Point {
         int x;
         int y;
@@ -18,11 +23,38 @@ namespace core {
             Entity(int x, int y, char character);
 
             Point getPosition();
-            //virtual bool move(Point to, Arena* arena) = 0;
+            virtual bool move(Point to, Arena* arena) = 0;
 
         private:
             Point position;
             char character; // the appearance of the entity
+    };
+
+    class Air : public Entity {
+        public:
+            Air(int x, int y) : Entity(x, y, ' ') {}
+
+            bool move(Point to, Arena* arena) override {
+                return false;
+            }
+    };
+
+    class Wall : public Entity {
+        public:
+            Wall(int x, int y) : Entity(x, y, 'X') {}
+
+            bool move(Point to, Arena* arena) override {
+                return false;
+            }
+    };
+
+    class MobWall : public Entity {
+        public:
+            MobWall(int x, int y) : Entity(x, y, 'M') {}
+
+            bool move(Point to, Arena* arena) override {
+                return false;
+            }
     };
 
 }
