9141764c8f14847794a203de34bb69f38703a5c4
Jacob Shing <zhanhoshing@icloud.com>
2025-04-17 21:26:21 +0800

    feat: implement file parsing logic for arena

--- src/core/arena_reader.cpp
+++ src/core/arena_reader.cpp
@@ -1,5 +1,8 @@
 #include <fstream>
 #include <string>
+#include <strstream>
+
+#include <core/arena.hpp>
 #include <core/arena_reader.hpp>
 
 namespace core {
@@ -17,45 +20,49 @@ namespace core {
     }
 
     bool ArenaReader::parseFile_() {
-        // TODO: implement the file parsing logic
-        /*
-            Format & Syntax of the file:
-                1. The size of the arena must be of the dimensions ARENA_WIDTH and ARENA_HEIGHT defined in core/arena.hpp.
-                2. Each line in the file represents a row of the arena. Each character in the line represents a pixel.
-                3. If there are more lines/characters than required, those extra lines/characters will be ignored.
-                4. If there are fewer lines/characters than required, the unknown pixels will be set to air.
-                5. ** The outermost layer of the arena MUST always be walls, regardless of the file content. ** (this is handled within the Arena already)
-                6. The characters in the file should be mapped like this:
-                    ' ' = air; 'X' = wall; 'P' = starting poing of the player;
-                7. The file MUST not contain mobs or other entities, otherwise a fail should be returned.
-        */
-        /* there are several parameters:
-        parse_success, which represents whether the file is valid and parsing can be done successfully.
-        custom_width and custom_height, which represents the width and height of the user_defined map.
-        The abovementioned two parameters need not be equal, larger, or smaller thatn ARENA_WIDth and ARENA_HEIGHT.
-        A comparison will be made to determine whether the user defined map is greater or smaller than the
-        predifined width and height.
-        arena =  new Arena() will be created and extra internal walls may be created
-        depending on whether the custom_width and custom_height is greater/smaller than
-        ARENA_WIDTH and ARENA_HEIGHT.
-        */
-        bool parse_success = true;
         if (!file->is_open()) {
-            parse_success = false;
+            errmsg = "Encountered issues opening the file.";
+            return false;
         }
-        else {
-            int custom_width, custom_height;
-            std::string mob;
-            while (file >> mob) {
-                if (mob != " " && mob != "X" && mob != "P"){
-                    parse_success = false;
-                    break;
+
+        arena = new Arena();
+        
+        bool playerFound = false;
+        std::string line;
+
+        for (int y = 0; y < ARENA_HEIGHT; ++y) {
+            if (!std::getline(*file, line)) line = std::string(ARENA_WIDTH, ' ');
+            if (line.length() < ARENA_WIDTH) line.append(ARENA_WIDTH - line.length(), ' ');
+            for (int x = 0; x < ARENA_HEIGHT; ++x) {
+                char c = line[x];
+                switch (c) {
+                    case 'X': // wall
+                        arena->SetPixel({x, y}, new Wall({x, y}, arena));
+                        break;
+                    case 'P': // player
+                        if (!playerFound) {
+                            arena->SetPixelWithId({x, y}, new Player({x, y}, arena));
+                            playerFound = true;
+                            break;
+                        }
+                        errmsg = "Invalid file syntax. More than one \'P\' found in the file. (line " + std::to_string(y) + ", column " + std::to_string(x) + ")";
+                        return false;
+                    case ' ': // air
+                        arena->SetPixel({x, y}, new Air({x, y}, arena));
+                        break;
+                    default:
+                        errmsg = "Invalid file syntax. Unknown character \'" + std::string(1, c) + "\' found in the file. (line " + std::to_string(y) + ", column " + std::to_string(x) + ")";
+                        return false;
                 }
             }
-            std::string line;
-            custom_width = (getline(file, line)).length();
         }
-        return parse_success;
+
+        if (!playerFound) {
+            errmsg = "Invalid file syntax. Expected exactly one \'P\' in the file. None found.";
+            return false;
+        }
+        
+        return true;
     }
 
     Arena* ArenaReader::GetArena() {
