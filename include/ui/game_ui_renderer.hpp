e35782258618170a260fd319fc09a5834e8471a7
Jacob Shing <zhanhoshing@icloud.com>
2025-04-18 00:30:48 +0800

    feat: implement GameUIRenderer class for rendering game UI

---
+++ include/ui/game_ui_renderer.hpp
@@ -0,0 +1,25 @@
+#ifndef UI_GAME_UI_RENDERER_HPP
+#define UI_GAME_UI_RENDERER_HPP
+
+#include <core/game.hpp>
+
+namespace ui {
+
+    //  Responsible for rendering the game UI.
+    class GameUIRenderer {
+        public:
+            //  Constructor
+            GameUIRenderer(core::Game* game);
+            //  Takes over ui::appScreen for rendering the game's UI.
+            //  This function must be called in the main thread, NOT from the tick thread.
+            void StartRenderLoop();
+            //  Refreshes the game UI. This method is thread-safe.
+            void RefreshUI();
+
+        private:
+            core::Game* game;
+    };
+    
+}
+
+#endif // UI_GAME_UI_RENDERER_HPP
---
+++ src/ui/game_ui_renderer.cpp
@@ -0,0 +1,42 @@
+#include <ui/common.hpp>
+#include <ui/game_ui_renderer.hpp>
+
+#include <util/log.hpp>
+
+#include <ftxui/component/component.hpp>
+
+namespace ui {
+
+    GameUIRenderer::GameUIRenderer(core::Game* game) : game(game) { }
+
+    void GameUIRenderer::StartRenderLoop() {
+        util::WriteToLog("Starting game UI renderer...", "GameUIRenderer::StartRenderLoop()");
+
+        auto container = ftxui::Container::Vertical({});
+        ui::RenderOption* entityRenderer = nullptr;
+        for (int y = 0; y < ARENA_HEIGHT; ++y) {
+            auto row = ftxui::Container::Horizontal({});
+            for (int x = 0; x < ARENA_WIDTH; ++x) {
+                entityRenderer = game->GetArena()->GetPixel({x, y})->GetRenderOption();
+                row->Add(ftxui::Renderer([&] {
+                    return entityRenderer->Render();
+                }));
+            }
+            container->Add(row);
+        }
+        auto ui = ftxui::Renderer(container, [&] {
+            return ftxui::vbox({
+                ftxui::text("Game Screen") | ftxui::center | ftxui::bold,
+                ftxui::separator(),
+                container->Render()
+            });
+        });
+        ui::appScreen.Loop(ui);
+    }
+
+    void GameUIRenderer::RefreshUI() {
+        util::WriteToLog("Refresh of game UI requested...", "GameUIRenderer::RefreshUI()");
+        ui::appScreen.Post(ftxui::Event::Custom);
+    }
+
+}
